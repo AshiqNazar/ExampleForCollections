08281792543f260fec139b04be43da0db2e0d072
AshiqNazar90 <khanashiq18@gmail.com>
2022-05-07 11:43:02 +0530

    object added

---
+++ ExampleForCollections/Groups.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleForCollections
+{
+ public class Groups
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public bool value { get; set; }
+        public double Mark { get; set; }
+
+    }
+
+}
--- ExampleForCollections/Program.cs
+++ ExampleForCollections/Program.cs
@@ -91,72 +91,83 @@ namespace ExampleForCollections
             //{
             //    Console.WriteLine(d.Key + " :" + d.Value);
             //}
-
-
-            List<int> lst = new List<int>();
-
-            lst.Add(100);
-            lst.Add(200);
-            lst.Add(300);
-            lst.Add(400);
-            lst.Add(500);
-            Console.WriteLine(lst.Capacity);
-            Console.WriteLine(lst.Capacity);
-            foreach (int item in lst)
+            Groups groups=new Groups();
+            List<Groups> grp = new List<Groups>
+            {new Groups{id=01, name="ashiq",value=true,Mark=49.5 },
+            new Groups{id=02, name="sam",value=false,Mark=75.7 },
+            new Groups{id=03, name="George",value=true,Mark=99.9 }
+            };
+           
+           
+            foreach(Groups g in grp)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(g.id+":"+g.name+ ":" +g.value+ ":" +g.Mark);
             }
+            
+            //List<int> lst = new List<int>();
+
+            //lst.Add(100);
+            //lst.Add(200);
+            //lst.Add(300);
+            //lst.Add(400);
+            //lst.Add(500);
+            //Console.WriteLine(lst.Capacity);
+            //Console.WriteLine(lst.Capacity);
+            //foreach (int item in lst)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
 
-            Dictionary<int, string> dct = new Dictionary<int, string>();
-            dct.Add(001, "Ashiq");
-            dct.Add(002, "vishnu");
-            dct.Add(003, "Aydu");
-            dct.Add(004, "Hashi");
-            foreach (KeyValuePair<int, string> kvp in dct)
-            {
-                Console.WriteLine(kvp.Key + " " + kvp.Value);
-                Console.WriteLine("\n");
-            }
+            //Dictionary<int, string> dct = new Dictionary<int, string>();
+            //dct.Add(001, "Ashiq");
+            //dct.Add(002, "vishnu");
+            //dct.Add(003, "Aydu");
+            //dct.Add(004, "Hashi");
+            //foreach (KeyValuePair<int, string> kvp in dct)
+            //{
+            //    Console.WriteLine(kvp.Key + " " + kvp.Value);
+            //    Console.WriteLine("\n");
+            //}
 
-            SortedList<int, string> sl = new SortedList<int, string>();
-            sl.Add(1, "oracle");
-            sl.Add(2, "vb.net");
-            sl.Add(3, "cs.net");
-            sl.Add(4, "asp.net");
+            //SortedList<int, string> sl = new SortedList<int, string>();
+            //sl.Add(1, "oracle");
+            //sl.Add(2, "vb.net");
+            //sl.Add(3, "cs.net");
+            //sl.Add(4, "asp.net");
 
-            foreach (KeyValuePair<int, string> kvp in sl)
-            {
-                Console.WriteLine(kvp.Key + " " + kvp.Value);
-                Console.Write("\n");
-            }
+            //foreach (KeyValuePair<int, string> kvp in sl)
+            //{
+            //    Console.WriteLine(kvp.Key + " " + kvp.Value);
+            //    Console.Write("\n");
+            //}
 
-            Queue<string> q = new Queue<string>();
+            //Queue<string> q = new Queue<string>();
 
-            q.Enqueue("cs.net");
-            q.Enqueue("vb.net");
-            q.Enqueue("asp.net");
-            q.Enqueue("sqlserver");
-            q.Dequeue();
-            foreach (string s in q)
-            {
-                Console.Write(s + "\n");
-            }
+            //q.Enqueue("cs.net");
+            //q.Enqueue("vb.net");
+            //q.Enqueue("asp.net");
+            //q.Enqueue("sqlserver");
+            //q.Dequeue();
+            //foreach (string s in q)
+            //{
+            //    Console.Write(s + "\n");
+            //}
 
 
-            Console.Write("\n");
+            //Console.Write("\n");
 
-            Stack<string> stk = new Stack<string>();
-            stk.Push("rahul");
-            stk.Push("sachin");
-            stk.Push("Ganguly");
-            stk.Push("Laxman");
-            stk.Pop();
+            //Stack<string> stk = new Stack<string>();
+            //stk.Push("rahul");
+            //stk.Push("sachin");
+            //stk.Push("Ganguly");
+            //stk.Push("Laxman");
+            //stk.Pop();
 
-            foreach (string s in stk)
-            {
-                Console.Write(s + "\n");
-            }
+            //foreach (string s in stk)
+            //{
+            //    Console.Write(s + "\n");
+            //}
             Console.ReadKey();
         }
     }
